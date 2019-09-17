1a56d18ade9f627b087bab82b1d07886a9212a48
Reimund <reimund@gmx.net>
2019-09-17 19:24:44 +0200

    bring 60Hz to work

--- test_serial/serial.js
+++ test_serial/serial.js
@@ -2,25 +2,25 @@ const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
 
 var parser = new Readline()
-var port =  new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
+var port =  new SerialPort('/dev/ttyACM0', { baudRate: 115200 });
 port.pipe(parser)
 parser.on('data', line => console.log(`<<< ${line}`))
 var x = 0;
 var r = 255
 var g = 0
 var b = 0
-setTimeout(()=>{setInterval(function() { sendNewColor(); }, 20);}, 3000);
-
+setTimeout(()=>{setInterval(function() { sendNewColor(); }, 16);}, 3000);
 
 function sendNewColor() {
-    var buffer = new Uint8Array(4);
-    buffer[0] = x;
-    buffer[1] = r;
-    buffer[2] = g;
-    buffer[3] = b;
+    var buffer = new Uint8Array(6);
+    buffer[0] = 1;
+    buffer[1] = x;
+    buffer[2] = r;
+    buffer[3] = g;
+    buffer[4] = b;
+    buffer[5] = 2;
     port.write(buffer);
-    // port.
-    console.log(">>> " + buffer)
+    // console.log(">>> " + buffer)
     if(x == 240) {
         if(r==255) { r=0; b=255;} else
         if(b==255) { b=0; g=255;} else
--- test_serial/sketch.ino
+++ test_serial/sketch.ino
@@ -2,13 +2,12 @@
 #ifdef __AVR__
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
-#define LED_PIN    3
+#define LED_PIN    2
 #define LED_COUNT 240
-int x;
-int red;
-int green;
-int blue;
-unsigned long ms;
+#define SERIAL_SPEED 115200
+#define SLEEP_TIME_TILL_SHOW_MS 500
+byte cmd[1];
+byte c[4];int x;int red;int green;int blue;unsigned long ms;unsigned long msc;
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -17,33 +16,26 @@ void setup() {
     clock_prescale_set(clock_div_1);
   #endif
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
-  // Serial.setTimeout(10);
+  strip.setPixelColor(5,255,0,0);         //  Set pixel's color (in RAM)
+  strip.show();
+  strip.setBrightness(1); // Set BRIGHTNESS to about 1/5 (max = 255)
   x = red = green = blue = 0;
-  ms = millis() + 100;
-  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+  ms = millis() + SLEEP_TIME_TILL_SHOW_MS;
+  Serial.begin(SERIAL_SPEED); // opens serial port, sets data rate to 9600 bps
   while (!Serial); // wait for serial port to connect. Needed for native USB port only
-  strip.setPixelColor(5, strip.Color(255,0,0));         //  Set pixel's color (in RAM)
 }
 
-byte c[4];
 void loop() {
-  // delay(1);
-  if (Serial.available() >= 4) {
-      // Serial.println("A " + String(Serial.available()));
-      Serial.readBytes(c, 4);
-      // Serial.println("L " + String(Serial.available()));
-      x = (int)c[0];
-      red = (int)c[1];
-      green = (int)c[2];
-      blue = (int)c[3];
-      Serial.println(String(x) + "," + String(red) + "," + String(green) +"," + String(blue));
-      // strip.setPixelColor(x, strip.Color(red,green,blue));         //  Set pixel's color (in RAM)
-  }
-  unsigned long msc = millis();
-  if(msc>ms) {
-      strip.show();
-      ms = msc + 100;
+  if (Serial.available() > 0) {
+      Serial.readBytes(cmd,1);
+      if((int)cmd[0] == 1) {
+        while (Serial.available() < 4);
+        Serial.readBytes(c, 4);
+        x = (int)c[0]; red = (int)c[1]; green = (int)c[2]; blue = (int)c[3];
+        // Serial.println(String(x) + "," + String(red) + "," + String(green) +"," + String(blue));
+        strip.setPixelColor(x,red,green,blue); // RAM
+      } else if ((int)cmd[0] == 2) {
+        strip.show();
+      }
   }
 }
