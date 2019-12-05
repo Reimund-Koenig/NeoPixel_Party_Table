9e6c7c1b281b7a4e8788e8bea54239199aea0695
Reimund König <reimund@gmx.net>
2019-12-05 22:18:34 +0100

    linting

--- server/controller/viewcontroller.js
+++ server/controller/viewcontroller.js
@@ -2,17 +2,18 @@ class viewcontroller {
     constructor(view, serial) {
         this.view = view;
         this.serial = serial;
-        }
+    }
     
     reset() {
+        console.log("Reset View (web and serial)")
         this.serial.setMatrixColor(0,0,0);
         this.view.setMatrixColor(0,0,0);
     }
 
     setMatrixColor(r,g,b) {
         console.log("Set Color " + r + ", "+ g + ", "+ b );
-        this.view.setMatrixColor(r,g,b);
-        this.serial.setMatrixColor(r,g,b);
+        this.view.setMatrixColor(r, g, b);
+        this.serial.setMatrixColor( r, g, b);
     }
     
     setColor(x,y,r,g,b) {
