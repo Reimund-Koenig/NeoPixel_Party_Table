27c8ada8d2a0a45abe3885cc44ad7ece5a1839dd
Reimund König <reimund@gmx.net>
2021-02-03 17:44:55 +0100

    refactor names part 2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const IP = require("ip");
 
-const View = require('./server/view/view')
 // const Serial = require('./server/controller/serial')
+const Playground = require('./user_interfaces/playground/web/playground')
+const Gamepad = require('./user_interfaces/gamepad/web/gamepad')
+const Debug_Information = require('./user_interfaces/debug_information/web/debug_information')
 const Viewcontroller = require('./control/viewcontroller')
-const Controller = require('./server/view/controller')
-const Menu = require('./server/view/menu')
 const AppManager = require('./control/app_mgr')
 
 var sizeX = 16;
@@ -12,10 +12,10 @@ var sizeY = 16;
 var FirstPersonControl = false;   // control command relative to the ingame players unit movement
 
 
-var view_website = new View(IP.address(), 3002, sizeX, sizeY);
+var view_website = new Playground(IP.address(), 3002, sizeX, sizeY);
 var viewcontroller = new Viewcontroller(view_website);
 var appManager = new AppManager(viewcontroller, sizeX, sizeY, FirstPersonControl);
 
 var self = this; // Start controller delayed, that app is already started
-setTimeout(function() { self.controller = new Controller(IP.address(), 3001, appManager); }, 2000);
-setTimeout(function() { self.menu = new Menu(IP.address(), 3003, appManager); }, 3000);
+setTimeout(function() { self.controller = new Gamepad(IP.address(), 3001, appManager); }, 2000);
+setTimeout(function() { self.menu = new Debug_Information(IP.address(), 3003, appManager); }, 3000);
