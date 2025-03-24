4699935815193e28e62307882fbcb497b81f771f
Reimund König <reimund@gmx.net>
2025-03-24 17:00:57 +0100

    fix high severity vulnerability: ip SSRF improper categorization in isPublic

--- table/index.js
+++ table/index.js
@@ -1,5 +1,17 @@
-const IP = require("ip");
+const os = require('os');
 
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+
+  for (const name in interfaces) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return "127.0.0.1";
+}
 const Playground = require('./user_interfaces/playground/web/playground')
 const Gamepad = require('./user_interfaces/gamepad/web/gamepad')
 const Debug_Information = require('./user_interfaces/debug_information/web/debug_information')
@@ -9,12 +21,12 @@ const AppManager = require('./control/app_mgr')
 var sizeX = 16;
 var sizeY = 16;
 var FirstPersonControl = false;   // control command relative to the ingame players unit movement
+var ip_address = getLocalIP()
 
-
-var web_playgound = new Playground(IP.address(), 3002, sizeX, sizeY);
+var web_playgound = new Playground(ip_address, 3002, sizeX, sizeY);
 var viewcontroller = new Viewcontroller(web_playgound);
 var appManager = new AppManager(viewcontroller, sizeX, sizeY, FirstPersonControl);
 
 var self = this; // Start delayed, that app is already started
-setTimeout(function() { self.gamepad = new Gamepad(IP.address(), 3001, appManager); }, 2000);
-setTimeout(function() { self.menu = new Debug_Information(IP.address(), 3003, appManager); }, 3000);
+setTimeout(function() { self.gamepad = new Gamepad(ip_address, 3001, appManager); }, 2000);
+setTimeout(function() { self.menu = new Debug_Information(ip_address, 3003, appManager); }, 3000);
